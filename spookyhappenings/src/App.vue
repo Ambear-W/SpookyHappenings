5d9f51089ecb52c26d2dbbbd28ea397f6ae7d150
Amber W <43912805+Ambear-W@users.noreply.github.com>
2021-09-06 15:11:17 -0400

    Testing things

--- spookyhappenings/src/App.vue
+++ spookyhappenings/src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Spooky Happenings!"/>
+  <HelloWorld msg="Welcome to Spooky Happenings"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
+  //anything related to vue goes here
   name: 'App',
   components: {
+    //the file that we imporated has been put in here --> we can now start referceing it in our template/site
     HelloWorld
   }
 }
